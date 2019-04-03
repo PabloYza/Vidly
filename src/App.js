3a48d00bbf0eba10bbff764c4fb35a33ffb96b22
Pablo <pablo.deyzaguirre@globalincubator.com>
2019-04-03 15:31:35 +0200

    Boiler plate + table of movies with fakeData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Movies from './components/movies'
 
 class App extends Component {
   state = {  }
   render() { 
     return (  
       <main className="container">
-        <h1>Vidly</h1>
+        <Movies />
       </main>
     );
   }
