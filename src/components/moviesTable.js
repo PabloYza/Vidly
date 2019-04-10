6c47892d60c10f786bb7c9e5124329a3ed86e41b
Pablo <pablo.deyzaguirre@globalincubator.com>
2019-04-10 10:12:24 +0200

    Refactoring + Rendering cell content

--- src/components/moviesTable.js
+++ src/components/moviesTable.js
@@ -9,8 +9,21 @@ class MoviesTable extends Component {
     { path: 'genre.name', label: 'Genre'},
     { path: 'numberInStock', label: 'Stock'},
     { path: 'dailyRentalRate', label: 'Rate'},
-    { key: 'like' },
-    { key: 'delete' }
+    { 
+      key: 'like', 
+      content: movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)}/> 
+    },
+    { 
+      key: 'delete',
+      content: movie => (
+        <button 
+          onClick={() => this.props.onDelete(movie)} 
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      )
+    }
   ];
   
   render() {
@@ -25,22 +38,8 @@ class MoviesTable extends Component {
         />
         <TableBody 
           data={movies}
-           
+          columns={this.columns}
         />
-        <tbody>
-          { movies.map(movie =>(
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like liked={movie.liked} onClick={() => onLike(movie)}/>
-              </td>
-              <td><button onClick={() => onDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-              </tr>
-            ))}
-        </tbody>
       </table>   
     );
   }
