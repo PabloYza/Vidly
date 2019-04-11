2b7ff8bda3c8640f3c6ed93a69384276bcb6fa42
Pablo <pablo.deyzaguirre@globalincubator.com>
2019-04-11 15:41:37 +0200

    Added Routes + Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Movies from './components/movies';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
+import Customers from './components/customers';
+import NavBar from './components/navBar';
+
+
 /* import Like from './components/common/like'; */
 
 class App extends Component {
   state = {  }
   render() { 
-    return (  
+    return (
+    <React.Fragment>
+      <NavBar />  
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path='/movies' component={Movies} ></Route>
+          <Route path='/customers' component={Customers} ></Route>
+          <Route path='/rentals' component={Rentals} ></Route>
+          <Route path='/not-found' component={NotFound} ></Route>
+          <Redirect from='/' exact to='/movies' />
+    {/* This Redirect will show the 'NOTFOUND' page when we type a route !== one of the above */}
+          <Redirect to='/not-found' />
+        </Switch>
       </main>
+    </React.Fragment>  
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 import App from "./App";
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.querySelector('#root')
-)
+);
