9197a62b4624c1a5d120e4c749f4254fdab95a0f
Pablo <pablo.deyzaguirre@globalincubator.com>
2019-04-11 16:46:57 +0200

    Bootstrap form + Handling formSubmission

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom'
 
 
+
 const NavBar = () => {
   return ( 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -28,6 +29,9 @@ const NavBar = () => {
           </NavLink>
           <NavLink className='nav-item nav-link' to='/rentals'>
             Rentals
+          </NavLink>
+          <NavLink className='nav-item nav-link' to='/login'>
+            Login
           </NavLink>
         </div>
       </div>
