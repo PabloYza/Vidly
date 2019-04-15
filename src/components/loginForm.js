92c2c55f4d19f042cc582b400d7bcc78f57ce4d1
Pablo <pablo.deyzaguirre@globalincubator.com>
2019-04-15 13:02:29 +0200

    Validation: Form and Field w/ Joi

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Input from './common/input';
+import Joi from 'joi-browser';
 
 class LoginForm extends Component {
   state = {
@@ -7,18 +8,40 @@ class LoginForm extends Component {
     errors: {}
   };
 
-  validate = () => {
-    const errors = {};
-
-    const { account } = this.state;
-    if (account.username.trim() === '')
-      errors.username = 'Username is required.'
-    if (account.password.trim() === '')
-      errors.password = 'Password is required.'
-
-// this method will return all the KEYS of this object in an array - Object.keys(targetObject)
-    return Object.keys(errors).length === 0 ? null : errors;
+  schema = {
+    username: Joi.string()
+      .required()
+      .label('Username'),
+    password: Joi.string()
+      .required()
+      .label('Password')
   };
+
+  validate = () => {
+    const options = { abortEarly: false };
+// 1st arg Object we want to validate, 2nd schema 
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
+    const errors = {};
+// How to map an Array into an Object
+    for (let item of error.details)
+      errors[item.path[0]] = item.message;
+    return errors;
+
+/* this method will return all the KEYS of this object in an array - Object.keys(targetObject)
+    return Object.keys(errors).length === 0 ? null : errors; */
+  };
+
+
+  validateProperty = ({ name, value }) => {
+// Whatever NAME is at runtime will be used to validate the KEY
+    const obj = { [name]: value };
+    const schema = {[name]: this.schema[name]};
+    const {error} = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
 
   handleSubmit = e => {
     //prevent the FullPage Reload
@@ -32,10 +55,16 @@ class LoginForm extends Component {
     console.log('Submitted');
   };
 
+
   handleChange = ({currentTarget: input}) => {
+    const errors = {...this.state.errors};
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = {...this.state.account};
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() { 
