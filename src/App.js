6986bf9ed790f406941c4e2a3e576bf0ad06c182
Pablo <pablo.deyzaguirre@globalincubator.com>
2019-06-13 15:27:51 +0200

    Authorization +show/hide elements based on user + Protecting routes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import MovieForm from './components/movieForm';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/common/registerForm';
 import Logout from './components/logout';
+import ProtectedRoute from './components/common/protectedRoute';
 /* Services */
 import auth from './services/authService';
 /* CSS modules */
@@ -26,21 +27,27 @@ class App extends Component {
     this.setState({ user });
   }
   
-  render() { 
+  render() {
+    const { user } = this.state;
+    
     return (
     <React.Fragment>
       <ToastContainer />
-      <NavBar user={this.state.user} />  
+      <NavBar user={user} />  
       <main className="container">
         <Switch>
           <Route path='/register' component={RegisterForm} />
           <Route path='/login' component={LoginForm} />
           <Route path='/logout' component={Logout} />
-          <Route path='/movies/:id' component={MovieForm} />
-          <Route path='/movies' component={Movies} ></Route>
-          <Route path='/customers' component={Customers} ></Route>
-          <Route path='/rentals' component={Rentals} ></Route>
-          <Route path='/not-found' component={NotFound} ></Route>
+        {/* This is done in order to "protect" a route */}
+          <ProtectedRoute path='/movies/:id' component={MovieForm} />
+          <Route 
+            path='/movies' 
+            render={props => <Movies {...props} user={user}/>} 
+          />
+          <Route path='/customers' component={Customers} />>
+          <Route path='/rentals' component={Rentals} />>
+          <Route path='/not-found' component={NotFound} />
           <Redirect from='/' exact to='/movies' />
     {/* This Redirect will show the 'NOTFOUND' page when we type a route !== one of the above */}
           <Redirect to='/not-found' />
